0f19af6b8901c4185ef5ac62b72e0b124f0b4a3d
7u4n1011 <juanherdie@gmail.com>
2025-05-19 09:04:15 -0300

    haciendo guia5

--- guia5/act1-5.py
+++ guia5/act1-5.py
@@ -4,7 +4,11 @@ def lista_random(n):
     return [random.randint(0,100) for _ in range(n)]
 
 lista1 = lista_random(10)
+print(f"Lista de ventas: {lista1}")
 
-i = 0
-while i < len(lista1):
-    
+ventas_ordenadas = sorted(lista1, reverse = True)
+mejores_tres = ventas_ordenadas[:3]
+print(f"Tres mejores ventas: {mejores_tres}")
+
+porcentajes = [venta * 0.10 for venta in mejores_tres]
+print(f"10% de las tres mejores ventas: {porcentajes}")
---
+++ guia5/act2-5.py
@@ -0,0 +1,15 @@
+# Creamos la funcion que nos pide el enunciado
+def ahorcado (letra, secret_w):
+    resultado = []
+    
+    for i in resultado:
+        if i == letra:
+            resultado.append(letra)
+        else:
+            resultado.append('_')
+    
+    return resultado
+
+palabra = 'ambulancia'
+print
+letra_ingresada
