c514d340824f3d9530a7d632dd7ccf52ffe117f1
7u4n1011 <juanherdie@gmail.com>
2025-05-26 08:37:08 -0300

    comenzando guia5

---
+++ guia6/act1-6.py
@@ -0,0 +1,9 @@
+def matriz_random(n,m):
+    return [[random.randint(0, 100) for _ in range(m)] for _ in range(n)]
+
+matriz1 = matriz_random(4, 4)
+
+for i in range (len(matriz1)):
+    for j in range(len(matriz1[0])):
+        if i == j:
+            print(matriz1[i][j])
